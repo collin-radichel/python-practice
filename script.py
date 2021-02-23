be67586b94a886f5a09a3fad15a0bb7f327c6577
collinradichel <radichel.collin@gmail.com>
2021-02-23 11:18:43 -0600

    initial commit, taking notes on python

---
+++ script.py
@@ -0,0 +1,36 @@
+
+# Comment Syntax
+# More Comments
+
+# Multi-line comments:
+
+"""
+MultiLine Comment
+
+print() - prints information
+
+variable naming convention: undercase_variable_names
+variables can have numbers but not at the beginning: variable_2
+
+
+
+ERRORS
+
+SyntaxError - means there is something wrong with the way your program is written —
+              punctuation that does not belong, a command where it is not expected,
+              or a missing parenthesis can all trigger a SyntaxError.
+
+NameError - occurs when the Python interpreter sees a word it
+            does not recognize. Code that contains something that
+            looks like a variable but was never defined will throw
+            a NameError.
+
+ZeroDivisionError - Python will raise this error when attempting to divide by 0
+
+
+DATA TYPES
+
+    int - whole numbers -2, -1, 0, 1, 2
+    float - fractional numbers 1.5, 20.668
+
+"""
